0077f6153c55ee7e0f06c672bb6bd6714691e59f
newmo13 <kubalipka123@gmail.com>
2025-01-24 19:04:04 +0100

    Initial commit

---
+++ src/main/java/dev/jlipka/notworkinglombok/NotWorkingLombokApplication.java
@@ -0,0 +1,13 @@
+package dev.jlipka.notworkinglombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotWorkingLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotWorkingLombokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jlipka/notworkinglombok/NotWorkingLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jlipka.notworkinglombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotWorkingLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
